c5d43d91e6604ec6594530ca82054124ad87e6da
jeanbrcho <jeancbs18@gmail.com>
2025-05-17 22:18:25 -0300

    cree el header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-import {
-  ContainerComponent,
-  DropdownComponent,
-  DropdownDividerDirective,
-  DropdownItemDirective,
-  DropdownMenuDirective,
-  DropdownToggleDirective,
-  HeaderBrandComponent,
-  HeaderComponent,
-  HeaderDividerComponent,
-  HeaderNavComponent,
-  HeaderTextComponent,
-  NavItemComponent,
-  NavLinkDirective
-} from '@coreui/angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    
-  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
