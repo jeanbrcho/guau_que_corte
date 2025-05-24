ebd7e80be83c47e8a14e107bc2980448404acf22
CLauchanchisan <claudia_casalone@hotmail.com>
2025-05-24 16:11:46 -0300

    rama provisional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+//hecho por mi
+import { BodyComponent } from './body/body.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet,HeaderComponent],
+    imports: [RouterOutlet,HeaderComponent, BodyComponent],
     templateUrl: './app.component.html',
     standalone: true,
     styleUrl: './app.component.css'
