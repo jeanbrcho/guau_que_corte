71808ce17aab22db1896fa381f4cc5d9ba264fe5
CLauchanchisan <claudia_casalone@hotmail.com>
2025-05-24 16:51:11 -0300

    body casi casi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet,HeaderComponent,FooterComponent],
+    imports: [RouterOutlet,HeaderComponent,FooterComponent,BodyComponent],
     templateUrl: './app.component.html',
     standalone: true,
     styleUrl: './app.component.css'
